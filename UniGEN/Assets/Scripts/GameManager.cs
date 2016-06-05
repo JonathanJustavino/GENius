957952a4bc3284bca4381513b9a6975234eee4c9
An0mal0caris <konrad@klinkosch.net>
2016-06-05 16:33:36 +0200

    A lot of stuff. All tools and their primary functions. Tweaks necessary.

--- UniGEN/Assets/Scripts/GameManager.cs
+++ UniGEN/Assets/Scripts/GameManager.cs
@@ -1,72 +1,97 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
 	private static GameManager instance;
-
-	private int cycles;
-	private bool dnaActive = false;
-	private bool menuActive = false;
-	private List<Plant> plants;
-
-	public int deadline;
-	public GameObject[] flowerPots;
-	public GameObject menu;
-	public GameObject dnaView;
-
-	public List<Plant> getPlantList{
-		get{
-			if(plants == null){
-				plants = new List<Plant>();
-			}
-			return plants;
-		}
-	}
-
-	public static GameManager Instance{
-		get{
-			if(instance == null){
+	public static GameManager Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
 				instance = FindObjectOfType<GameManager>();
 			}
 			return instance;
 		}
 	}
 
+	private int cycles;
+	private bool dnaActive = false;
+	private bool menuActive = false;
+
+	[SerializeField]
+	private GameObject[] flowerSlots;
+	[SerializeField]
+	private int deadline;
+
+	public GameObject menu;
+	public GameObject dnaView;
+
+	public string[][] seedPool { get; set; }
+	public Sprite defaultPlantSprite;
+	public Sprite defaultSeedSprite;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		cycles = deadline;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
-	}
-		
-	public void endCycle(){
-		
+	void Update()
+	{
+
 	}
 
-	public void restart(){
-		
+	public void endCycle()
+	{
+		cycles--;
+		Slot s;
+		foreach (GameObject go in flowerSlots)
+		{
+			s = go.GetComponent<Slot>();
+			if (s.plant != null)
+				s.plant.GetComponent<Plant>().grow();
+		}
 	}
 
-	public void exit(){
-		
+	public void restart()
+	{
+
 	}
 
-	public void toggleSound(){
-		if(menu != null){
+	public void exit()
+	{
+
+	}
+
+	public void toggleSound()
+	{
+		if (menu != null)
+		{
 			menuActive = !menuActive;
-			menu.SetActive(menuActive);	
+			menu.SetActive(menuActive);
 		}
 	}
 
-	public void toggleDNA(Plant flowerPower){
-		if(dnaView != null){
+	public void toggleDNA(Plant flowerPower)
+	{
+		if (dnaView != null)
+		{
 			dnaActive = !dnaActive;
-			dnaView.SetActive(dnaActive);	
+			dnaView.SetActive(dnaActive);
 		}
 	}
 
+	public Sprite getPhenotype(string[] genotype)
+	{
+		return defaultPlantSprite;
+	}
+
+	public Sprite getSeedImage()
+	{
+		return defaultSeedSprite;
+	}
 }
--- UniGEN/Assets/Scripts/Tool.cs
+++ UniGEN/Assets/Scripts/Tool.cs
@@ -1,15 +1,55 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class Tool : MonoBehaviour {
+public class Tool : MonoBehaviour
+{
+	private Image myImage;
 
-	// Use this for initialization
-	void Start () {
-	
+	public bool isActive { get; private set; }
+
+	public Sprite defaultImage;
+	public Sprite activeImage;
+
+	public Texture2D cursor;
+	public Texture2D defaultGameCursor { get; set; }
+	public Vector2 cursorHotSpot;
+
+
+	protected virtual void Awake()
+	{
+		myImage = GetComponent<Image>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void SetActive()
+	{
+		isActive = true;
+		myImage.sprite = activeImage;
+		Cursor.SetCursor(cursor, cursorHotSpot, CursorMode.Auto);
+	}
+
+	public void Reset()
+	{
+		isActive = false;
+		myImage.sprite = defaultImage;
+	}
+
+
+	void Update()
+	{
+		if (isActive)
+		{
+			if (Input.GetButton("Fire2"))
+			{
+				Reset();
+				Cursor.SetCursor(defaultGameCursor, cursorHotSpot, 0);
+			}
+			else toolUpdate();
+		}
+	}
+
+	protected virtual void toolUpdate()
+	{
+
 	}
 }
