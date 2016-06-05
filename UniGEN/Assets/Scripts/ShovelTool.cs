2ad22dcae3eed8b5dd89a922c32e94d78f0dc1eb
An0mal0caris <konrad@klinkosch.net>
2016-06-05 21:44:45 +0200

    Revert "Revert "Some stuff rearranged""
    This reverts commit 8acdb7da71e8cee5316984c7746b783e04ccc92e.

--- UniGEN/Assets/Scripts/ShovelTool.cs
+++ UniGEN/Assets/Scripts/ShovelTool.cs
@@ -19,10 +19,10 @@ public class ShovelTool : Tool
 			else if (hit.collider.gameObject.tag == "Slot")
 			{
 				slot = hit.collider.GetComponent<Slot>();
-				if (slot.plant != null)
+				if (slot.PlantObject != null)
 				{
-					target = slot.plant;
-					slot.plant = null;
+					target = slot.PlantObject;
+					slot.PlantObject = null;
 				}
 				else target = null;
 			}
@@ -40,23 +40,23 @@ public class ShovelTool : Tool
 			hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
 			if (hit.collider == null || hit.collider.tag != "Slot")
 			{
-				slot.plant = target;
+				slot.PlantObject = target;
 				target.transform.position = slot.transform.position;
 				target = null;
 			}
 			else
 			{
 				Slot mySlot = hit.collider.GetComponent<Slot>();
-				if (mySlot.plant != null)
+				if (mySlot.PlantObject != null)
 				{
-					slot.plant = target;
+					slot.PlantObject = target;
 					target.transform.position = slot.transform.position;
 					target = null;
 				}
 				else
 				{
 					slot = mySlot;
-					slot.plant = target;
+					slot.PlantObject = target;
 					target.transform.position = slot.transform.position;
 					target = null;
 				}
