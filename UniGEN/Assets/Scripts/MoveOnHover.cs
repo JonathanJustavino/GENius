7e95a7f9df20cd00fce12dafb9a92c1de72d84b0
Jeannine <jeannineborn@aol.com>
2016-06-08 16:40:23 +0200

    Added MouseOver & Move for Horse

---
+++ UniGEN/Assets/Scripts/MoveOnHover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class MoveOnHover : MonoBehaviour {
+    public Transform startPos;
+    public Transform endPos;
+    public Movement movement;
+    public float animationTime = 1;
+
+    private Collider2D col;
+
+    void Awake()
+    {
+        if (movement == null)
+            this.enabled = false;
+        col = GetComponent<Collider2D>();
+        StartCoroutine(mouseCheck());
+    }
+	
+    IEnumerator mouseCheck()
+    {
+        while(true)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward, 15f);
+            if (hit && hit.collider == col)
+            {
+                if (transform != endPos && !movement.routineRunning)
+                    movement.move(endPos.position, 1);
+
+            }
+            else
+            {
+                if (transform != startPos && !movement.routineRunning)
+                    movement.move(startPos.position, 1);
+            }
+            yield return null;
+        }
+    }
+}
