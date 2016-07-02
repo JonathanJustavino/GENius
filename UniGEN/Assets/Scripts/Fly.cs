e66f6176a6e30f71e47bc4a1f3a7028adb447cc1
Jeannine <jeannineborn@aol.com>
2016-07-02 18:34:58 +0200

    Add Anfangsszene

---
+++ UniGEN/Assets/Scripts/Fly.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly : MonoBehaviour {
+
+    public Transform collectable;
+    public Transform startTransform;
+    public Transform endTransform;
+    public float moveSpeed;
+    private Vector3 direction;
+    private Transform destination;
+    // Use this for initialization
+
+    void Start()
+    {
+        SetDestination(startTransform);
+    }
+
+    void FixedUpdate()
+    {
+        collectable.GetComponent<Rigidbody>().MovePosition(collectable.position + direction * moveSpeed * Time.fixedDeltaTime);
+
+        if (Vector3.Distance(collectable.position, destination.position) < moveSpeed * Time.fixedDeltaTime)
+        {
+            SetDestination(endTransform);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(endTransform.position, collectable.localScale);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(startTransform.position, collectable.localScale);
+    }
+
+    void SetDestination(Transform dest)
+    {
+        destination = dest;
+        direction = (destination.position - collectable.position).normalized;
+    }
+
+
+}
